216c280a35314121de9c191f2a8935e803f85e52
Дмитрий Шашев <DmitryShashev@outlook.com>
2022-12-06 16:19:46 +0700

    merge two repositories

--- src/BrandUp.CardDav.Server/IResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BrandUp.CardDav.Server.Abstractions
-{
-    public interface IResult
-    {
-        public bool IsSuccess { get; }
-        public bool IsError { get; }
-        public string ErrorMessage { get; }
-    }
-}
