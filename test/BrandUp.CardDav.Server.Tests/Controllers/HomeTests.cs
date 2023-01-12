8de12599554aaf75e5484da1a688a937ee731563
Дмитрий Шашев <DmitryShashev@outlook.com>
2023-01-12 15:25:09 +0700

    Comment test

--- test/BrandUp.CardDav.Server.Tests/Controllers/HomeTests.cs
+++ test/BrandUp.CardDav.Server.Tests/Controllers/HomeTests.cs
@@ -1,6 +1,4 @@
 using BrandUp.CardDav.Server.Controllers.Tests;
-using BrandUp.CardDav.Transport.Models.Headers;
-using BrandUp.CardDav.Transport.Models.Requests;
 using Xunit.Abstractions;
 
 namespace BrandUp.CardDav.Server.Tests.Controllers
@@ -23,13 +21,13 @@ namespace BrandUp.CardDav.Server.Tests.Controllers
             Output.WriteLine(string.Join(" ", options.DavHeaderValue));
         }
 
-        [Fact]
-        public async Task Success_WellKnown()
-        {
-            var options = await Client.PropfindAsync("/.well-known/carddav", PropfindRequest.AllProp(Depth.One), CancellationToken.None);
+        //[Fact]
+        //public async Task Success_WellKnown()
+        //{
+        //    var options = await Client.PropfindAsync("/.well-known/carddav", PropfindRequest.AllProp(Depth.One), CancellationToken.None);
 
-            Output.WriteLine(options.StatusCode.ToString());
-            Assert.True(options.IsSuccess);
-        }
+        //    Output.WriteLine(options.StatusCode.ToString());
+        //    Assert.True(options.IsSuccess);
+        //}
     }
 }
