a2a2dead440d6c9f19d41202abb50894c03c9517
Дмитрий Шашев <DmitryShashev@outlook.com>
2023-01-19 12:00:46 +0700

    Removed needless folders

--- src/BrandUp.CardDav.Transport.Binding/Exceptions/BindingException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BrandUp.CardDav.Transport.Binding.Exceptions
-{
-    public class BindingException : Exception
-    {
-        public BindingException(string message) : base(message)
-        { }
-    }
-}
--- src/BrandUp.CardDav.Transport.Binding/Exceptions/XmlDeserializeException.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BrandUp.CardDav.Transport.Binding.Exceptions
-{
-    public class XmlDeserializeException : Exception
-    {
-        public XmlDeserializeException(string message) : base(message) { }
-    }
-}
